f98eaca1eb776e99b6492cbba5af66f73f8a666b
abuckenberger <abuckenberger@eagles.ewu.edu>
2017-03-13 14:10:46 -0700

    PLUGIN
    *plugin works
    *added scss file
    *yo it works homie

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 // YOUR SCRIPTS GO HERE
+
+//go get the weather 
+
+$.simpleWeather({
+ location: 'los angeles',
+ unit: 'f', 
+ success: function(weather){
+   console.log(weather);
+   
+   $('.temp').text(weather.temp);
+   $('.city').text(weather.city);
+   $('.state').text(weather.region);
+   $('.image img').attr('src',      weather.image);
+   
+ },
+ error: function(error){
+   console.log('can i have the wifi');
+ }
+  
+});
+
+
