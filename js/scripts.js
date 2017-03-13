a616738ebbee5e68dcbba102ce139b7aaaec9ab4
abuckenberger <abuckenberger@eagles.ewu.edu>
2017-03-13 16:29:48 -0700

    added forecast figure
    *added figure to hold forecast

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,9 @@
 
 //go get the weather 
 
+
 $.simpleWeather({
- location: 99004,
+ location: 'los angeles',
  unit: 'f', 
  success: function(weather){
    console.log(weather);
@@ -13,11 +14,19 @@ $.simpleWeather({
    $('.state').text(weather.region);
    $('.image img').attr('src',      weather.image);
    
+   //future weather day 1
+  $('.forecast figure:nth-child(1) img').attr('src', weather.forecast[0].image);
+   
+   //future weather caption day 1
+   
+   $('.forecast figure:nth-child(1) figcaption').text(weather.forecast[0].text);
+   
  },
  error: function(error){
-   console.log('can i have the wifi');
+   console.log('can i have the wifi plz');
  }
   
 });
+
 
 
